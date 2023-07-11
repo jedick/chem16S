0b8e7bbe9f05e875007981ff38147e2a7ec8dfec
jedick <j3ffdick@gmail.com>
2023-07-11 11:50:31 +0800

    Resubmission to CRAN

--- inst/tinytest/test_phyloseq_integration.R
+++ inst/tinytest/test_phyloseq_integration.R
@@ -1,5 +1,7 @@
 # Test added on 20230705
 
+if(requireNamespace("phyloseq", quietly = TRUE)) {
+
 info <- "Different metrics can be chosen in ps_metrics()"
 data(mouse.GTDB)
 metrics <- c("GRAVY", "pI")
@@ -10,3 +12,5 @@ info <- "A single metric is still returned in a data frame"
 metrics <- "Zc"
 values <- ps_metrics(mouse.GTDB, metrics = metrics, quiet = TRUE)
 expect_equal(colnames(values), metrics, info = info)
+
+}
