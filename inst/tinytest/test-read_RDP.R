d9cbea6b11af698999d8df64e42e9d1eb21d5d79
jedick <j3ffdick@gmail.com>
2025-03-21 14:14:11 +0800

    Add more tests for read_RDP() and phyloseq integration

--- inst/tinytest/test-read_RDP.R
+++ inst/tinytest/test-read_RDP.R
@@ -37,3 +37,15 @@ expect_identical(unique(RDP.phylum$rank), "phylum", info = info)
 info <- "drop.groups = FALSE (the default) includes domain-level classifications"
 RDP.phylum <- read_RDP(file, lowest.level = "phylum", quiet = TRUE)
 expect_equal(unique(RDP.phylum$rank), c("phylum", "domain"), info = info)
+
+# 20250321
+info <- "'lineage' argument works as expected"
+RDP.archaea <- read_RDP(file, lineage = "Archaea", quiet = TRUE)
+expect_length(grep("Bacteria", RDP.archaea$lineage), 0, info = info)
+
+info <- "Error if no samples have at least 'mincount' counts"
+expect_error(read_RDP(file, lineage = "Euryarchaeota", quiet = TRUE), "No samples are left!", info = info)
+info <- "More samples remain by lowering 'mincount'"
+RDP.eury10 <- read_RDP(file, lineage = "Euryarchaeota", quiet = TRUE, mincount = 10)
+RDP.eury1 <- read_RDP(file, lineage = "Euryarchaeota", quiet = TRUE, mincount = 1)
+expect_true(ncol(RDP.eury1) > ncol(RDP.eury10), info = info)
--- inst/tinytest/test_phyloseq_integration.R
+++ inst/tinytest/test_phyloseq_integration.R
@@ -28,3 +28,18 @@ info <- "plot_ps_metrics2() returns plotting object with expected content"
 p2 <- plot_ps_metrics2(GlobalPatterns, color = "SampleType", refdb = "RefSeq_206", quiet = TRUE)
 expect_equal(names(p2$data)[1:2], c("Zc", "nH2O"), info = info)
 expect_equal(dim(p2$data), c(26, 10), info = info)
+
+# 20250321
+info <- "plot_ps_metrics() handles 'color' and 'shape' arguments"
+p3 <- plot_ps_metrics(GlobalPatterns, x = "SampleType", color = "SampleType", sortby = "Zc", refdb = "RefSeq_206")
+expect_equal(p3$labels$colour, "SampleType", info = info)
+p4 <- plot_ps_metrics(GlobalPatterns, x = "SampleType", shape = "SampleType", sortby = "Zc", refdb = "RefSeq_206")
+expect_equal(p4$labels$shape, "SampleType", info = info)
+
+info <- "plot_ps_metrics2() stops for too few metrics and warns for too many"
+expect_error(plot_ps_metrics2(GlobalPatterns, metrics = c("Zc"), color = "SampleType", refdb = "RefSeq_206", quiet = TRUE),
+  "please supply the names of two metrics", info = info)
+expect_warning(plot_ps_metrics2(GlobalPatterns, metrics = c("Zc", "nH2O", "nO2"), color = "SampleType", refdb = "RefSeq_206", quiet = TRUE),
+  "using the first two", info = info)
+
+dev.off()
