df51f65f63e3cd091bcc55db25ca93dc4e1afc9e
jedick <j3ffdick@gmail.com>
2025-09-13 18:43:42 +0800

    Fix tests for ggplot2 4.0.0

--- inst/tinytest/test-phyloseq_integration.R
+++ inst/tinytest/test-phyloseq_integration.R
@@ -19,8 +19,11 @@ pdf(NULL)
 info <- "plot_ps_metrics() returns plotting object with expected content"
 data(GlobalPatterns, package = "phyloseq")
 p1 <- plot_ps_metrics(GlobalPatterns, x = "SampleType", sortby = "Zc", refdb = "RefSeq_206")
-expect_equal(p1$labels$x, "SampleType", info = info)
-expect_equal(p1$labels$y, "Chemical Metric", info = info)
+# Build the plot object to get labels 20250913
+# Needed after https://github.com/tidyverse/ggplot2/pull/5879
+p1_b <- ggplot2::ggplot_build(p1)
+expect_equal(p1_b$plot$labels$x, "SampleType", info = info)
+expect_equal(p1_b$plot$labels$y, "Chemical Metric", info = info)
 expect_equal(dim(p1$data), c(78, 10), info = info)
 expect_equal(levels(p1$data$variable), c("Zc", "nO2", "nH2O"), info = info)
 
@@ -32,9 +35,11 @@ expect_equal(dim(p2$data), c(26, 10), info = info)
 # 20250321
 info <- "plot_ps_metrics() handles 'color' and 'shape' arguments"
 p3 <- plot_ps_metrics(GlobalPatterns, x = "SampleType", color = "SampleType", sortby = "Zc", refdb = "RefSeq_206")
-expect_equal(p3$labels$colour, "SampleType", info = info)
+p3_b <- ggplot2::ggplot_build(p3)
+expect_equal(p3_b$plot$labels$colour, "SampleType", info = info)
 p4 <- plot_ps_metrics(GlobalPatterns, x = "SampleType", shape = "SampleType", sortby = "Zc", refdb = "RefSeq_206")
-expect_equal(p4$labels$shape, "SampleType", info = info)
+p4_b <- ggplot2::ggplot_build(p4)
+expect_equal(p4_b$plot$labels$shape, "SampleType", info = info)
 
 info <- "plot_ps_metrics2() stops for too few metrics and warns for too many"
 expect_error(plot_ps_metrics2(GlobalPatterns, metrics = c("Zc"), color = "SampleType", refdb = "RefSeq_206", quiet = TRUE),
